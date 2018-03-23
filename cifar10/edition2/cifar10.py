545ab451c61522d05467ab5e613b9da2d48df79c
NowGood <m13261701238@163.com>
2018-03-23 08:42:38 +0800

    :)

--- cifar10/edition2/cifar10.py
+++ cifar10/edition2/cifar10.py
@@ -48,7 +48,7 @@ import cifar10_input
 FLAGS = tf.app.flags.FLAGS
 
 # Basic model parameters.
-tf.app.flags.DEFINE_integer('batch_size', 512,
+tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
 tf.app.flags.DEFINE_string('data_dir', 'D:\ComputeVision\cifar10_data',
                            """Path to the CIFAR-10 data directory.""")
@@ -64,7 +64,7 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.99    # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 50.0      # Epochs after which learning rate decays.
+NUM_EPOCHS_PER_DECAY = 200.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
 INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 
